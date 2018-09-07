35b3f783e9821f061e1b65e160d05ef5f16dfaa3
sameen73 <lightwave73@gmail.com>
2018-09-07 15:04:21 -0400

    I wrote a new sum function
    This is only a test.

---
+++ new_sum.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+x = 5
+y = 11
+def new_sum(x, y):
+    """
+    Sums two numbers together
+    
+    Args:
+        x: A number.
+        y: Another number.
+    
+    Returns:
+        The sum of x and y
+    """
+    return x+y
+
+z = new_sum(x,y)
